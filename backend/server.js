e3234a4377dd9f603cdd72348d70ca26050b503c
Surendra Gahlot <surendragahlot57@gmail.com>
2023-04-23 23:54:10 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("api running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  res.send(note);
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is started on ${port}`);
+});
