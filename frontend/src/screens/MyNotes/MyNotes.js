d6ab20f399531e30057068d9ce87792d69254d5d
Surendra Gahlot <surendragahlot57@gmail.com>
2023-04-26 16:52:48 +0530

    addind edit and delete mynotes page build

--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,10 +1,77 @@
 import React from "react";
 import MainScreen from "../../component/MainScreen";
+import { Link } from "react-router-dom";
+import { Badge, Button, Card, Accordion } from "react-bootstrap";
+import { useAccordionButton } from "react-bootstrap/AccordionButton";
+import notes from "../../data/notes";
+
+function CustomToggle({ children, eventKey }) {
+  const decoratedOnClick = useAccordionButton(eventKey, () =>
+    console.log("totally custom!")
+  );
+
+  return <h5 onClick={decoratedOnClick}>{children}</h5>;
+}
 
 const MyNotes = () => {
+  const deleteHandler = () => {
+    if (window.confirm("Are you sure ??")) {
+    }
+  };
   return (
     <>
-      <MainScreen title="Welcome Back Surendra Gahlot..">My Notes</MainScreen>
+      <MainScreen title="Welcome Back Surendra Gahlot..">
+        <Link to="createnote">
+          <Button style={{ marginLeft: 10, marginBottom: 6 }} size="lg">
+            Create New Note
+          </Button>
+        </Link>
+        {notes.map((note) => {
+          return (
+            <Accordion defaultActiveKey="0" flush>
+              <Card style={{ margin: 10 }}>
+                <Card.Header style={{ display: "flex" }}>
+                  <span
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18,
+                    }}
+                  >
+                    <CustomToggle eventKey="0">{note.title}</CustomToggle>
+                  </span>
+                  <div>
+                    <Button href={`/note/${note._id}`}>Edit</Button>
+                    <Button
+                      variant="danger"
+                      className="mx-2"
+                      onClick={() => deleteHandler(note._id)}
+                    >
+                      Delete
+                    </Button>
+                  </div>
+                </Card.Header>
+                <Accordion.Collapse eventKey="0">
+                  <Card.Body>
+                    <h4>
+                      <Badge bg="success">Category - {note.category}</Badge>
+                    </h4>
+                    <blockquote className="blockquote mb-0">
+                      <p>{note.content}</p>
+                      <footer className="blockquote-footer">
+                        Created On - date
+                      </footer>
+                    </blockquote>
+                  </Card.Body>
+                </Accordion.Collapse>
+              </Card>
+            </Accordion>
+          );
+        })}
+      </MainScreen>
     </>
   );
 };
